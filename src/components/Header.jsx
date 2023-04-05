f7e59bf890d56eff940c96dfe1caac5748a7ee12
Artem047 <artem.glebov2004@yandex.ru>
2023-04-05 15:52:43 +0300

    fst commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+    return <header className='header'>
+        <img src='https://i.pinimg.com/564x/94/4a/d7/944ad708cb3d18bde746fbc753c5b16d.jpg' />
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://mobimg.b-cdn.net/v3/fetch/8a/8a2e15f705cb2619322527ea48640a54.jpeg?w=1200&r=0.5625' />
+        </div>
+        <div>
+            ava + descriptions
+        </div>
+        <div>
+            My posts
+            <div>
+                New posts
+            </div>
+            <div>
+                <div>
+                    post1
+                </div>
+                <div>
+                    post2
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
