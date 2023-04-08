01868ea007d1ada9a421b159b0322f1ae05bba3b
Artem047 <artem.glebov2004@yandex.ru>
2023-04-08 21:57:10 +0300

    lesson 16

--- src/components/Profile/MyPosts/Posts/Post.jsx
+++ src/components/Profile/MyPosts/Posts/Post.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
     return (
         <div className={s.item}>
             <img src='https://i.pinimg.com/564x/3d/68/54/3d6854e69b2efc6fb44fee0252ab4da4.jpg' />
-            post1
+            {props.message}
             <div>
                 <span>like</span>
             </div>
