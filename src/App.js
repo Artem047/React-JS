f600eb9e73d537f0e253fef73cd5b0c4436fe63a
Artem047 <artem.glebov2004@yandex.ru>
2023-04-09 14:20:25 +0300

    lesson 19

--- src/App.js
+++ src/App.js
@@ -4,19 +4,31 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+import News from './components/News/News';
+import Settings from './components/Settin/Settings';
+import Music from './components/Music/Music';
 
 
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      <div className='app-wrapper-content'>
-        <Dialogs />
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Navbar />
+        <div className='app-wrapper-content'>
+          <Routes>
+            <Route path='/dialogs' Component={Dialogs} />
+            <Route path='/profile' Component={Profile} />
+            <Route path='/news' Component={News}/>
+            <Route path='/music' Component={Music}/>
+            <Route path='/settings' Component={Settings}/>
+          </Routes>
+        </div>
       </div>
-      {/* <Profile /> */}
-    </div>
+    </BrowserRouter>
   );
 }
 
