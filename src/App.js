297ed98adb55be980f9c542c22edbfba6d13fe92
Artem047 <artem.glebov2004@yandex.ru>
2023-04-09 15:30:02 +0300

    lesson 20

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Dialogs from './components/Dialogs/Dialogs';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Routes } from 'react-router-dom';
 import News from './components/News/News';
-import Settings from './components/Settin/Settings';
+import Settings from './components/Settings/Settings';
 import Music from './components/Music/Music';
 
 
