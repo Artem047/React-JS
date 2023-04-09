c02801f978242278fdc81109edc0a5b82ed097df
Artem047 <artem.glebov2004@yandex.ru>
2023-04-09 13:33:49 +0300

    lesson 18

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import s from './Profile.module.css'
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img src='https://i.pinimg.com/564x/c7/57/b9/c757b95adf617a20ea8cb82a5c35967e.jpg' />
             </div>
