de41d91809b8cfbc1028c38e02a6a65f5737f46e
Artem047 <artem.glebov2004@yandex.ru>
2023-04-20 15:33:59 +0300

    lesson 27

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
@@ -12,7 +11,7 @@ import Music from './components/Music/Music';
 
 
 
-const App = () => {
+const App = (props) => {
   return (
     <BrowserRouter>
       <div className='app-wrapper'>
