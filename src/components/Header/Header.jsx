f1480f5f08c4f13aa3fc1eefecc0c68127840d5f
Artem047 <artem.glebov2004@yandex.ru>
2023-04-21 20:13:05 +0300

    lesson 33

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import s from './Header.module.css'
 
 const Header = () => {
     return <header className={s.header}>
-        <img src='https://i.pinimg.com/564x/94/4a/d7/944ad708cb3d18bde746fbc753c5b16d.jpg' />
+        <img />
     </header>
 }
 
--- src/redux/state.jsx
+++ src/redux/state.jsx
@@ -1,3 +1,4 @@
+import { rerenderEntireTree } from "../render";
 
 let state = {
     profilePage:{
@@ -31,6 +32,7 @@ export let addPost = (postMessage) => {
     };
 
     state.profilePage.posts.push(newPost)
+    rerenderEntireTree(state); 
 }
 
 
