3fab8f01ffd7a576b8f2e7d589663a96e44d5d2c
Artem047 <artem.glebov2004@yandex.ru>
2023-04-21 16:09:03 +0300

    lesson 31

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,20 +1,34 @@
 import s from './Dialogs.module.css'
 import DialogItem from './DialogItem/DialogItem';
 import Message from './Message/Message';
+import React from 'react';
 
 const Dialogs = (props) => {
 
-    
-    let dialogsElements = props.state.dialogs.map( d => <DialogItem name={d.name} id={d.id} />); 
-    let messagesElements = props.state.messages.map( m => <Message message={m.message} />)
 
+    let dialogsElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id} />);
+    // let messagesElements = props.state.messages.map( m => <Message message={m.message} />)
+
+
+    let newMessageElement = React.createRef();
+
+    let addMessage = ()=>{
+        let text = newMessageElement.current.value;
+        alert(text)
+    }
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 {dialogsElements}
             </div>
             <div className={s.messages}>
-                {messagesElements}
+                {/* {messagesElements} */}
+                <div>
+                    <textarea rows={5} ref={newMessageElement}></textarea>
+                </div>
+                <div>
+                    <button onClick={addMessage}>Add message</button>
+                </div>
             </div>
         </div>
     )
