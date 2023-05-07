0e0720889fb7045631766af8da794333f81a661a
Artem047 <artem.glebov2004@yandex.ru>
2023-05-07 12:01:52 +0300

    lesson 38

--- src/redux/state.jsx
+++ src/redux/state.jsx
@@ -1,56 +1,54 @@
 let store = {
-    _state: {
-    profilePage:{
-        posts: [
-            { id: 1, message: 'Hi, how are you?', likesCount: 12 },
-            { id: 2, message: 'It is my first post', likesCount: 11 },
-        ],
-        newPostText: 'Artem Glebov'
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 1, message: "Hi, how are you?", likesCount: 12 },
+        { id: 2, message: "It is my first post", likesCount: 11 },
+      ],
+      newPostText: "Artem Glebov",
     },
-    dialogsPage:{
-    messages:[
-        { id: 1, message: 'Hello' },
-        { id: 2, message: 'How are you?' },
-        { id: 3, message: 'Hi' }
-    ],
-    dialogs:[
-        { id: 1, name: 'Artem' },
-        { id: 2, name: 'Andrey' },
-        { id: 3, name: 'Sveta' },
-        { id: 4, name: 'Sasha' },
-        { id: 5, name: 'Viktor' }
-    ]
+    dialogsPage: {
+      messages: [
+        { id: 1, message: "Hello" },
+        { id: 2, message: "How are you?" },
+        { id: 3, message: "Hi" },
+      ],
+      dialogs: [
+        { id: 1, name: "Artem" },
+        { id: 2, name: "Andrey" },
+        { id: 3, name: "Sveta" },
+        { id: 4, name: "Sasha" },
+        { id: 5, name: "Viktor" },
+      ],
     },
-    sidebar:{}
-},
-_callSubscriber(){
-    console.log('State changed');
-},
-getState(){
-    return this._state
-},
-rerenderEntireTree() {
-    console.log('state is changed');
-},
-addPost(){
-    let newPost = {
-        id: 5, 
-        message: this._state.profilePage.newPostText,
-        likesCount: 0
-    };
+    sidebar: {},
+  },
+  _callSubscriber() {
+    console.log("State changed");
+  },
 
-    this._state.profilePage.posts.push(newPost);
-    this._state.profilePage.newPostText = '';
-    this._callSubscriber(this._state); 
-},
-updateNewPostText(newText){
-    this._state.profilePage.newPostText = newText
-    this._callSubscriber(this._state); 
-},
-subscribe(observer){
+  getState() {
+    return this._state;
+  },
+  subscribe(observer) {
     this._callSubscriber = observer;
-}
+  },
 
-}
+  dispatch(action) {
+    if (action.type === "ADD-POST") {
+      let newPost = {
+        id: 5,
+        message: this._state.profilePage.newPostText,
+        likesCount: 0,
+      };
+      this._state.profilePage.posts.push(newPost);
+      this._state.profilePage.newPostText = "";
+      this._callSubscriber(this._state);
+    } else if (action.type === "UPDATE-NEW-POST-TEXT") {
+      this._state.profilePage.newPostText = action.newText;
+      this._callSubscriber(this._state);
+    }
+  },
+};
 export default store;
 window.store = store;
